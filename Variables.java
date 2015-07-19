a370fbf0364bc74a25a768ed7c61339aea17a582
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-18 23:56:00 -0400

    Update and rename README.md to Variables.java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Pennies{
+	public static void main(String args[]){
+		double pennies;
+		pennies = 8;
+		System.out.print("I have ");
+		System.out.print(pennies);
+		System.out.println(" pennies");
+		System.out.print("..yay..");
+	}
+}
